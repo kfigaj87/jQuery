1f44f289368d3e036b6c244d68060ba956944aea
kfigaj <kfigaj87@gmail.com>
2022-09-18 10:12:54 +0200

    zakonczenie warsztatu Accordion

--- 4-accordion-jquery/js/main.js
+++ 4-accordion-jquery/js/main.js
@@ -1,1 +1,13 @@
+$(document).ready(function () {
+  $(".accordion-item").click(function () {
+    console.log($(this).hasClass("open"));
 
+    if ($(this).hasClass("open")) {
+      $(this).removeClass("open");
+      $(this).find(".accordion-content").slideUp();
+    } else {
+      $(this).addClass("open");
+      $(this).find(".accordion-content").slideDown();
+    }
+  });
+});
